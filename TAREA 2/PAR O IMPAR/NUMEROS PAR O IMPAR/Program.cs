216b17f9841a1a11a24177985207d85de43fc752
adrieljr08 <adrieljuniorguzmanbautista@gmail.com>
2025-06-06 03:20:16 -0400

    Add files via upload

---
+++ TAREA 2/PAR O IMPAR/NUMEROS PAR O IMPAR/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace paroimpar
+{
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingrese un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine(" El numero es par");
+            }
+            else
+            {
+                Console.WriteLine(" El numero es impar");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
